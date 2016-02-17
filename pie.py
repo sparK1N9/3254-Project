8a10ef37ee51fbe4aeca9e8ee5689c3bcd13810c
sparK1N9 <spacecqcenhancer@gmail.com>
2016-02-17 03:13:27 -0500

    redirect

--- pie.py
+++ pie.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['post', 'get'])
 def someFunc():
-    return render_template("main.html");
+    if request.method=='POST':
+        return render_template("main.html");
+    return render_template("log in.html");
 
 app.run("127.0.0.1", 13000, debug=True)
