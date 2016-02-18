1721f2ad124a8ba2bff14295effddbb113c1540c
sparK1N9 <spacecqcenhancer@gmail.com>
2016-02-17 21:40:45 -0500

    sign up and log in implemented

--- pie.py
+++ pie.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+users = {} #sorry for using a global
 
 @app.route("/", methods=['post', 'get'])
-def someFunc():
+def func():
     if request.method=='POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        if request.form["action"]=='signUp':
+            users[username]=password
+        else:
+            if username not in users: return 'user name not found.'
+            if password != users[username]: return 'password not correct.'
         return render_template("main.html");
     return render_template("log in.html");
 
