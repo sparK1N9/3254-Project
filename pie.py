258e75c0fad4247495f82abd7b9d106710b8c009
sparK1N9 <spacecqcenhancer@gmail.com>
2016-02-21 15:49:35 -0500

    update

--- pie.py
+++ pie.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 users = {} #sorry for using globals
-loggedIn = 0
 
 @app.route("/", methods=['post', 'get'])
-def func():
+def logIn():
     if request.method=='POST':
         username = request.form["username"]
         password = request.form["password"]
@@ -14,18 +13,22 @@ def func():
             else: return 'You have already signed up.'
         else:
             if username not in users: return 'user name not found.'
-            if password != users[username]: return 'password not correct.'
-        loggedIn = 1
-        return redirect('/user/<username>')
+            if password != users[username]: return 'password not correct.'     
+        return redirect(url_for('main',username = request.form["username"]));
     return render_template("log in.html");
 
 @app.route('/logout/', methods=['post', 'get'])
-def func2():
-    loggedIn = 0
+def logOut():
     return 'You have logged out.'
 
-@app.route('/user/<username>/', methods=['post', 'get'])
-def func3():
-    return render_template("main.html",user = username, loggedIn = loggedIn);
+@app.route('/user/<username>', methods=['post', 'get'])
+def main(username):
+    if request.method=='POST':
+        if request.form["log out"]:
+            return redirect('/logout/')
+        if request.form["delete account"]:
+            del users[username]
+            return 'Account deleted.'
+    return render_template("main.html",user = username);
 
 app.run("127.0.0.1", 13000, debug=True)
