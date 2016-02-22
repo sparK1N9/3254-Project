3450f4f9040501178e78fcd9068c2bc5fe9b5564
sparK1N9 <spacecqcenhancer@gmail.com>
2016-02-21 22:20:11 -0500

    phase I complete

--- pie.py
+++ pie.py
@@ -9,13 +9,15 @@ def logIn():
         username = request.form["username"]
         password = request.form["password"]
         if request.form["action"]=='signUp':
-            if username not in users: users[username]=password
+            if username not in users:
+                userData = [password,[],[]]
+                users[username] = userData
             else: return 'You have already signed up.'
         else:
             if username not in users: return 'user name not found.'
-            if password != users[username]: return 'password not correct.'
+            if password != users[username][0]: return 'password not correct.'
         session['username'] = request.form["username"]
-        return redirect(url_for('main',username = request.form["username"]));
+        return redirect(url_for('home',username = request.form["username"]));
     return render_template("log in.html");
 
 @app.route('/logout/')
@@ -24,16 +26,35 @@ def logOut():
     return 'You have logged out.'
 
 @app.route('/user/<username>', methods=['post', 'get'])
-def main(username):
+def home(username):
     if 'username' not in session: return 'log in first!'
     if request.method=='POST':
         if "log out" in request.form:
-            return redirect(url_for('logOut', username = username))
+            return redirect(url_for('logOut'))
         elif "del acc" in request.form:
             session.pop('username', None)
             del users[username]
             return 'Account deleted.'
+        elif 'p' in request.form:
+            msg = request.form["msg"]
+            users[username][1].append(msg)
+        elif 'search' in request.form:
+            target = request.form["target"]
+            if target not in users: return 'user not found.'
+            if target in users[username][2]: return 'already following this person.'
+            users[username][2].append(target)
+        elif 'unfollow' in request.form:
+            person = request.form["person"]
+            if person not in users[username][2]: return 'not following this user.'
+            users[username][2].remove(person)
+            return 'unfollow sucessful.'
+        elif 'posts' in request.form:
+            lst = []
+            lst += users[username][1]
+            for otherUser in users[username][2]:
+                lst += users[otherUser][1]
+            return render_template("posts.html",seq = lst)
     return render_template("main.html",user = username);
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'A0Zr98j/3yX R~XHH!jN]LWX/,?RT'
 app.run("127.0.0.1", 13000, debug=True)
